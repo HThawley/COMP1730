717d10783b3aac1b337938acbb0e177248191088
HThawley <Hmthawley@gmail.com>
2020-10-11 21:28:32 +1100

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 11 20:22:28 2020
-
-@author: hmtha
-"""
-
 import csv
 
 with open("aqi_data_civic.csv") as file:
@@ -20,22 +13,26 @@ for row in raw_data:
 data.sort()
 
 year = int(data[0][0])
-month = int(data[0][1])
-day = int(data[0][2])
+month = 0
+day = 0
 count = 0
 
 for row in data: 
-    if row[4] == '':
+    if row[4] == '' or int(row[4]) <100:
         pass
-    elif int(row[4]) >= 100: 
+    else:
         if int(row[0]) == year:
-            if int(row[2]) == day:
-                if int(row[1]) != month:
+            if int(row[1]) == month:
+                if int(row[2]) == day:
+                    pass
+                else:
                     count += 1
+                    print(row[1:3])
                     day = int(row[2])
                     month = int(row[1])
             else:
                 count +=1 
+                print(row[1:3])
                 day = int(row[2])
                 month = int(row[1])
         else:
@@ -44,18 +41,4 @@ for row in data:
             year += 1 
             month = 0
             day=0
-            if int(row[0]) == year:
-                if int(row[2]) != day:
-                    count += 1
-                    day = int(row[2])  
-           
-PM_100 = []
-for x in data:
-    if x[4] == '':
-        pass
-    elif int(x[4]) >= 100: 
-        PM_100.append(x) 
-print(len(PM_100))
-
-
-    
+print("In " + str(year) + " there were " + str(count) + " days with PM2.5 above 100")
